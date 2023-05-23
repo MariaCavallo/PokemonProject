683017e949a9b9c99c7acb5ef726c17d793dea35
Maria Cavallo <mariacavallom@gmail.com>
2023-05-22 21:27:14 -0300

    update docs url

--- src/urls.js
+++ src/urls.js
@@ -1,2 +1,2 @@
 export const githubURL = "https://github.com/MariaCavallo/SpecializationFrontend"
-export const docsURL = "https://pokemon-center.vercel.app/"
+export const docsURL = "https://pokemon-center-docs.vercel.app/"
