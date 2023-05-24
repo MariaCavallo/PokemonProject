bf9626475dbb70b68cc760bdb4bb0db1b17cead0
Maria Cavallo <mariacavallom@gmail.com>
2023-05-24 09:59:10 -0300

    change url scheme in FormContext.jsx

--- src/context/ContextoFormulario.jsx
+++ src/context/ContextoFormulario.jsx
@@ -6,7 +6,7 @@
  * 
  * #### Utilización en el proyecto
  * Aquí vemos un diagrama de la forma que estamos usando este contexto.
- * ![diagrama formcontext](https://github.com/MariaCavallo/SpecializationFrontend/blob/main/src/assets/formcontext.png)
+ * ![diagrama formcontext](https://raw.githubusercontent.com/MariaCavallo/SpecializationFrontend/main/src/assets/formcontext.png)
  * @module ContextoFormulario
  */
 
